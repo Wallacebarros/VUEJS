9151d10833cc0fc00e4bb7803809f21853048154
Wallace Barros <w.wallacebarros.cruz@gmail.com>
2024-03-21 14:16:16 -0300

    reatividade usando o reactive

--- myapp/src/views/CompositionApiView.vue
+++ myapp/src/views/CompositionApiView.vue
@@ -1,13 +1,32 @@
 <template>
-  {{ name }}
+  <p>fixed variable: {{ name }}</p>
+  <br/>
+  <h3>reactive variables</h3>
+  <p>{{ user.firts_name }} {{ user.last_name}}</p>
+
+  <button @click='changeLastName'>
+    change last name
+  </button>
 </template>
 <script>
+  import { reactive } from 'vue'
+  
   export default {
     setup() {
       const name = 'Jhon Jep'
+      
+      const user = reactive({
+        firts_name:'Jhon',
+        last_name:'Jep'
+      })
 
+      const changeLastName = ()=>{
+        user.last_name = 'new last name'
+      }
       return {
-        name
+        name,
+        user,
+        changeLastName
       }
     }
   }
