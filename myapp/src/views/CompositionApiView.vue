27f248730d50a5c61d6db951161aa6bdc68acee7
Wallace Barros <w.wallacebarros.cruz@gmail.com>
2024-03-21 19:20:00 -0300

    crated watch and computed in compositionAPI

--- myapp/src/views/CompositionApiView.vue
+++ myapp/src/views/CompositionApiView.vue
@@ -3,13 +3,15 @@
   <br/>
   <h3>reactive variables</h3>
   <p>{{ user.firts_name }} {{ user.last_name}}</p>
-
   <button @click='changeLastName'>
     change last name
   </button>
+  <h3>computed in coposition api</h3>
+  {{ fullName }}
+
 </template>
 <script>
-  import { ref } from 'vue'
+  import { ref, computed, watch } from 'vue'
   
   export default {
     setup() {
@@ -20,13 +22,22 @@
         last_name:'Jep'
       })
 
+      const fullName = computed(()=>{
+        return `${user.value.firts_name} ${user.value.last_name}`
+      })
+
+      watch( ()=>user.value.last_name, ()=>{
+        console.log('altered')
+      })
+
       const changeLastName = ()=>{
         user.value.last_name = 'new last name'
       }
       return {
         name,
         user,
-        changeLastName
+        changeLastName,
+        fullName
       }
     }
   }
